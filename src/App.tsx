71fc0ca0b0c821da5c978d416a2faac5786c840c
Richard Mörbitz <richard.moerbitz@tu-dresden.de>
2024-08-28 22:12:31 +0200

    Add initial prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
+
+import { H5PPlayerUI, H5PEditorUI } from '@lumieducation/h5p-react';
+
 import logo from './logo.svg';
 import './App.css';
 
+import { ContentService } from './services/ContentService';
+
+const ContentDump = (props: any) => {
+  const [content, setContent] = React.useState(
+    "{}"
+  );
+  const editor = props.editor;
+  const contentService = props.contentService;
+
+  return (
+    <div>
+      <p>{content}</p>
+      <button type="button" onClick={() => {console.log(contentService.editorContent); editor.current?.save(); setContent(contentService.editorContent);}}>Save</button>
+    </div>
+  );
+};
+
 function App() {
+  const contentService = new ContentService('/h5p');
+  const h5pEditor: React.RefObject<H5PEditorUI> = React.createRef();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <H5PEditorUI
+        contentId='new'
+        ref={h5pEditor} 
+        loadContentCallback = {contentService.getEdit}
+        saveContentCallback = {contentService.save}
+        onSaved = { (contentId: string, metadata: any) => {console.log(contentId);} }
+        //onSaveError = { (message: string) => {console.log(message);} }
+      />
+        <ContentDump editor={h5pEditor} contentService={contentService}/>
     </div>
   );
 }
