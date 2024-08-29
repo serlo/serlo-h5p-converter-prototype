fccd6767bebf7c47f1068736b23bd10867df5db1
Stephan Kulla <git.mail@kulla.me>
2024-08-29 21:13:42 +0200

    Changes of our call

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,50 @@
-import React from 'react';
+import React from "react";
 
-import { H5PPlayerUI, H5PEditorUI } from '@lumieducation/h5p-react';
+import { H5PPlayerUI, H5PEditorUI } from "@lumieducation/h5p-react";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import { ContentService } from './services/ContentService';
+import { ContentService } from "./services/ContentService";
 
 const ContentDump = (props: any) => {
-  const [content, setContent] = React.useState(
-    "{}"
-  );
+  const [content, setContent] = React.useState("{}");
   const editor = props.editor;
   const contentService = props.contentService;
 
   return (
     <div>
-      <p>{content}</p>
-      <button type="button" onClick={() => {console.log(contentService.editorContent); editor.current?.save(); setContent(contentService.editorContent);}}>Save</button>
+      <pre>{JSON.stringify(JSON.parse(content), null, 2)}</pre>
+      <button
+        type="button"
+        onClick={() => {
+          console.log(contentService.editorContent);
+          editor.current?.save();
+          setContent(contentService.editorContent);
+        }}
+      >
+        Save
+      </button>
     </div>
   );
 };
 
 function App() {
-  const contentService = new ContentService('/h5p');
+  const contentService = new ContentService("/h5p");
   const h5pEditor: React.RefObject<H5PEditorUI> = React.createRef();
   return (
     <div className="App">
       <H5PEditorUI
-        contentId='new'
-        ref={h5pEditor} 
-        loadContentCallback = {contentService.getEdit}
-        saveContentCallback = {contentService.save}
-        onSaved = { (contentId: string, metadata: any) => {console.log(contentId);} }
+        contentId="new"
+        ref={h5pEditor}
+        loadContentCallback={contentService.getEdit}
+        saveContentCallback={contentService.save}
+        onSaved={(contentId: string, metadata: any) => {
+          console.log(contentId);
+        }}
         //onSaveError = { (message: string) => {console.log(message);} }
       />
-        <ContentDump editor={h5pEditor} contentService={contentService}/>
+      <ContentDump editor={h5pEditor} contentService={contentService} />
     </div>
   );
 }
